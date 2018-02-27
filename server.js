6d15f057756c2cef9b7b3438e5800fc829d489d2
John Paul Depew <jpdepewjp@gmail.com>
2018-02-27 17:26:54 -0500

    Changed listen port to 3001

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.get('/about', function(req, res) {
     res.render('pages/about');
 });
 
-app.listen(8080);
-console.log('8080 is the magic port');
+app.listen(3001);
+console.log('3001 is the magic port');
