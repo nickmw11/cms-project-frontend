08d948014e3edeed1830d9fd2387a796d7252b72
nickmw11 <nickmwinter@gmail.com>
2018-04-09 01:23:08 -0400

    Added a test image in to the articles page to see how it looks

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/article', function(req, res) {
         numRows = result.length;
         var articleArray = [];
         for (i = numRows - 1; i >= 0; i--) {
-        resultString = resultString + "<h2>" + result[i].Title + "</h2><h3>" + "Author: " + result[i].Author + "</h3>" + '<p>' + result[i].Content + '</p>' + result[i].Date + '<br>';
+        resultString = resultString + "<div class=\"container\"> <div class=\"row\"> <div class=\"col-sm-4\"><h2>" + result[i].Title + "</h2><h3>" + "Author: " + result[i].Author + "</h3>" + '<p>' + result[i].Content + '</p>' + result[i].Date + '</div><div class="col-sm-4"><img src="test.png" width="175" height="175"></div></div></div><br><br>';
       }
         res.send(resultString);
     });
