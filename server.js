f6e50d60f9c7926307e47fdec02867f885e28820
Nick Winter <nickmwinter@gmail.com>
2018-04-12 13:46:59 -0400

    Adding images from database to front end

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/article', function(req, res) {
         numRows = result.length;
         var articleArray = [];
         for (i = numRows - 1; i >= 0; i--) {
-        resultString = resultString + "<div class=\"container\"> <div class=\"row\"> <div class=\"col-sm-8\"><h2>" + result[i].Title + "</h2><h3>" + "Author: " + result[i].Author + "</h3>" + '<p>' + result[i].Content + '</p>' + result[i].Date + '</div><div class="col-sm-4"><img src="test.png" width="175" height="175"></div></div></div><br><br>';
+        resultString = resultString + "<div class=\"container\"> <div class=\"row\"> <div class=\"col-sm-8\"><h2>" + result[i].Title + "</h2><h3>" + "Author: " + result[i].Author + "</h3>" + '<p>' + result[i].Content + '</p>' + result[i].Date + '</div><div class="col-sm-4"><img src="' + result[i].Image + '" width="175" height="175"></div></div></div><br><br>';
       }
         res.send(resultString);
     });
