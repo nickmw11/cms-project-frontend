9c16bfb77edcf3ed7d0741284968fe9a4321c803
Nick Winter <nickmwinter@gmail.com>
2018-04-18 01:55:38 -0400

    Working on displaying images in articles

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -12,9 +12,11 @@ exports.displayArticles = function (req, res){
         mysqlConnect.query(query, function (err, result, fields) {
             if (err) throw err;
 
+
             numRows = result.length;
             var articleArray = [];
             for (i = numRows - 1; i >= 0; i--) {
+
             resultString = resultString + "<div class=\"container\"> <div class=\"row\"> <div class=\"col-sm-8\"><h2>" + result[i].Title + "</h2><h3>" + "Author: " + result[i].Author + "</h3>" + '<p>' + result[i].Content + '</p>' + result[i].Date + '</div><div class="col-sm-4"><img src="' + result[i].Image + '" width="175" height="175"></div></div></div><br><br>';
         }
         res.send(resultString);
