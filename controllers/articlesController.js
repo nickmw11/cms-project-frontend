69a8db8a8e5a8a3620c0f4589104c3276013e180
John Paul Depew <jpdepewjp@gmail.com>
2018-04-19 14:25:12 -0400

    Front end only pulls articles with is_active set to 1

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -16,9 +16,11 @@ exports.displayArticles = function (req, res){
             numRows = result.length;
             var articleArray = [];
             for (i = numRows - 1; i >= 0; i--) {
-
-            resultString = resultString + "<div class=\"container\"> <div class=\"row\"> <div class=\"col-sm-8\"><h2>" + result[i].Title + "</h2><h3>" + "Author: " + result[i].Author + "</h3>" + '<p>' + result[i].Content + '</p>' + result[i].Date + '</div><div class="col-sm-4"><img src="' + result[i].Image + '" width="175" height="175"></div></div></div><br><br>';
-        }
+                if(result[i].is_active == 1)
+                {
+                    resultString = resultString + "<div class=\"container\"> <div class=\"row\"> <div class=\"col-sm-8\"><h2>" + result[i].Title + "</h2><h3>" + "Author: " + result[i].Author + "</h3>" + '<p>' + result[i].Content + '</p>' + result[i].Date + '</div><div class="col-sm-4"><img src="' + result[i].Image + '" width="175" height="175"></div></div></div><br><br>';
+                }
+            }
         res.send(resultString);
     });
 }
