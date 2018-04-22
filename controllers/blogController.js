0fc33937a8d975d7f34675d6f08a3c2cd6b657db
John Paul Depew <jpdepewjp@gmail.com>
2018-04-22 15:33:37 -0400

    Blog controller checks for is_active before displaying blog

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -10,15 +10,18 @@ exports.displayBlog = function(req, res){
     var resultString = "";
 
     mysqlConnect.query(query, function (err, result, fields) {
-            if (err) throw err;
+        if (err) throw err;
 
-            // Pulls info from blog database table
+        // Pulls info from blog database table
 
-            numRows = result.length;
-            var blogArray = [];
-            for (i = numRows - 1; i >= 0; i--) {
-            resultString = resultString + "<div class=\"container\"> <div class=\"row\"> <div class=\"col-sm-8\"><h2>" + result[i].Title + "</h2><h3>" + "Author: " + result[i].Author + "</h3>" + '<p>' + result[i].Content + '</p>' + result[i].Date + '</div><div class="col-sm-4"><img src="test.png" width="175" height="175"></div></div></div><br><br>';
+        numRows = result.length;
+        var blogArray = [];
+        for (i = numRows - 1; i >= 0; i--) {
+            if(result[i].is_active == 1)
+            {
+                resultString = resultString + "<div class=\"container\"> <div class=\"row\"> <div class=\"col-sm-8\"><h2>" + result[i].Title + "</h2><h3>" + "Author: " + result[i].Author + "</h3>" + '<p>' + result[i].Content + '</p>' + result[i].Date + '</div><div class="col-sm-4"><img src="test.png" width="175" height="175"></div></div></div><br><br>';
+            }
         }
             res.send(resultString);
-        });
+    });
 };
