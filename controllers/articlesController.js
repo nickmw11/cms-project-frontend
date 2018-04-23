db25a055c21a7468eabf011270f3c529c03221d9
Nick Winter <nickmwinter@gmail.com>
2018-04-23 14:36:42 -0400

    Finished image display

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -7,6 +7,15 @@ var mysqlConnect = require('../config/database.js');
 var request = require('request');
 var fs = require('fs');
 var http = require('http');
+const { URL } = require('url');
+            //  This function downloads the necessary images from the back end site
+              function downloadFile (imageName){
+
+                var file = fs.createWriteStream("public/" + imageName);
+                var req = http.get("http://localhost:3000/" + imageName, function(res) {
+                  res.pipe(file);
+              });
+              }
 
 exports.displayArticles = function (req, res){
         var query = "Select * from articles"
@@ -19,23 +28,14 @@ exports.displayArticles = function (req, res){
             numRows = result.length;
             var articleArray = [];
             for (i = numRows - 1; i >= 0; i--) {
-              //  This function downloads files from the public folder on the back end, and allows them to be displayed on the front end site
-              //  Currently a WIP - very slow and images won't display until this function is moved out of loop.
-              //  Need to add error handling - currently front end will crash if back end isn't also running
-              //  - Nick
-              function downloadFile (){
-
-                var file = fs.createWriteStream("public/" + result[i].Image);
-                var req = http.get("http://localhost:3000/" + result[i].Image, function(res) {
-                  res.pipe(file);
-              });
-              }
-              // To turn on file transfer from back end to front end, uncomment below line
-              // downloadFile();
 
                 if(result[i].is_active == 1)
                 {
-                  console.log(result[i].Image);
+                    if (!fs.existsSync('./public/' + result[i].Image))
+                    {
+                      downloadFile(result[i].Image);
+                      console.log("Downloading " + result[i].Image);
+                    }
                     resultString = resultString + "<div class=\"container\"> <div class=\"row\"> <div class=\"col-sm-8\"><h2>" + result[i].Title + "</h2><h3>" + "Author: " + result[i].Author + "</h3>" + '<p>' + result[i].Content + '</p>' + result[i].Date + '</div><div class="col-sm-4"><img src="' + result[i].Image + '" width="175" height="175"></div></div></div><br><br>';
                 }
             }
